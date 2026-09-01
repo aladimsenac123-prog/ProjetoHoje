f799cb12a7792ad03f9ad77a7649129d74e662a2
aladimsenac123-prog <aladimsenac123@gmail.com>
2026-09-01 17:28:54 -0300

    Fix GitHub Pages asset paths

--- sw.js
+++ sw.js
@@ -2,17 +2,10 @@ const CACHE_NAME = 'ecoentregas-v1';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
-  '/css/variables.css',
   '/css/main.css',
-  '/css/animations.css',
   '/css/responsive.css',
-  '/js/main.js',
-  '/js/components/mobile-menu.js',
-  '/js/components/co2-counter.js',
-  '/js/components/scroll-reveal.js',
   '/js/integrations/google-sheets.js',
-  '/js/utils/validators.js',
-  '/js/utils/helpers.js'
+  '/js/utils/validators.js'
 ];
 
 self.addEventListener('install', (event) => {
